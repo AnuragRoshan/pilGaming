f4955df8f91fae3169c1736208fb51bc3bd3adb3
Anurag Roshan <anuragraushan373@gmail.com>
2024-09-22 11:02:06 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,10 @@ import Stopwatch from "./components/stopwatch";
 import WeatherApp from "./components/weather";
 
 function App() {
-  // By default, Stopwatch is shown
   const [showComponent, setShowComponent] = useState<string>("stopwatch");
 
   return (
     <div className="App">
-      {/* Buttons to toggle between Stopwatch and WeatherApp */}
       <div className="flex justify-center space-x-4 my-4">
         <button
           className={`px-4 py-2 rounded-md font-bold ${
@@ -33,7 +31,6 @@ function App() {
         </button>
       </div>
 
-      {/* Conditional rendering of components based on button click */}
       <div className="content">
         {showComponent === "stopwatch" && <Stopwatch />}
         {showComponent === "weather" && <WeatherApp />}
